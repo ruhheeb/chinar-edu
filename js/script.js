6a83f18e63217488e51cc959527b2857dfe853e8
Rahib Babayev <rahibwebdeveloper@gmail.com>
2020-09-16 19:28:55 +0400

    create edited all blocks and added responsive 1440

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 $(document).ready(function () {
+  // Main training Video
+  var videoButton = $('.video__button');
+  var closeButton = $('.video__close-button');
+  videoButton.on('click', openVideo);
+  closeButton.on('click', closeVideo);
 
+  function openVideo(event) {
+    event.preventDefault();
+    var videoTraining = $('.video__wrap');
+    $(videoTraining).addClass('video__wrap--visible');
+  };
+
+  function closeVideo(event) {
+    event.preventDefault();
+    var videoTraining = $('.video__wrap');
+    $(videoTraining).removeClass('video__wrap--visible');
+  };
+
+  $(document).keyup(function (event) {
+    if (event.key === 'Escape' || event.keyCode === 27) {
+      closeVideo(event);
+    }
+  });
+
+  // Training Slider
   var trainingSlider = new Swiper('.training-slider__container', {
     effect: 'coverflow',
     grabCursor: true,
@@ -8,7 +32,7 @@ $(document).ready(function () {
     coverflowEffect: {
       rotate: 0,
       stretch: 340,
-      depth: 400,
+      depth: 300,
       modifier: 1,
       slideShadows: true,
     },
